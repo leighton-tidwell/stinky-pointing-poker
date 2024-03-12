4384bbe7f8b43c4eac3854e0c3aaab6745d619d3
Leighton Tidwell <leighton@tidwells.world>
2024-03-12 17:31:59 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { cn } from "@/lib/utils";
 import { Viewport } from "next";
@@ -24,6 +25,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
