5e3579786b76817c621be7f51873619b93f7172d
Bryce Hull <bahull@live.com>
2024-06-01 20:08:37 -0500

    Implemented shadcn cards, shadcn collapsible and removed padding

--- src/components/story-points-key-table.tsx
+++ src/components/story-points-key-table.tsx
@@ -1,4 +1,15 @@
-import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 const storyPointDescriptions = [
   {
@@ -35,100 +46,92 @@ const storyPointDescriptions = [
 ];
 
 export const StoryPointsKeyTable = () => {
-  const [isDefinitionCollapsed, setIsDefinitionCollapsed] = useState(true);
-  const [isTableCollapsed, setIsTableCollapsed] = useState(true);
-
-  const toggleDefinitionCollapse = () => {
-    setIsDefinitionCollapsed(!isDefinitionCollapsed);
-  };
-
-  const toggleTableCollapse = () => {
-    setIsTableCollapsed(!isTableCollapsed);
-  };
-
   return (
-    <div className="overflow-x-auto p-4">
-      <div className="mb-4 rounded-lg bg-gray-100 p-4">
-        <button
-          className="mb-2 w-full text-left text-xl font-bold focus:outline-none"
-          onClick={toggleDefinitionCollapse}
-          aria-expanded={!isDefinitionCollapsed}
-          aria-controls="definition-content"
-        >
-          Definition of Done {isDefinitionCollapsed ? "+" : "-"}
-        </button>
-        <div id="definition-content" hidden={isDefinitionCollapsed}>
-          <p className="text-sm text-gray-700">
-            The "Definition of Done" is a shared understanding among the team of
-            what it means for work to be considered complete. This includes the
-            completion of all tasks, successful passing of all tests, and
-            adherence to all relevant standards and guidelines. This ensures
-            consistency and quality in the deliverables.
-          </p>
-          <p className="mt-2 text-sm text-gray-700">
-            For example, a feature is considered done when:
-            <ul className="list-inside list-disc">
-              <li>All linting, unit and integration tests are passing.</li>
-              <li>
-                A merge request (MR) is created, reviewed, and approved through
-                Peer and Lead Review.
-              </li>
-              <li>
-                The code is merged into the master branch or a dedicated epic
-                branch.
-              </li>
-            </ul>
-          </p>
-        </div>
-      </div>
-      <div className="mb-4 rounded-lg bg-gray-100 p-4">
-        <button
-          className="mb-2 w-full text-left text-xl font-bold focus:outline-none"
-          onClick={toggleTableCollapse}
-          aria-expanded={!isTableCollapsed}
-          aria-controls="table-content"
-        >
-          Story Point Descriptions {isTableCollapsed ? "+" : "-"}
-        </button>
-        <div id="table-content" hidden={isTableCollapsed}>
-          <table className="min-w-full border-collapse bg-white">
-            <thead>
-              <tr>
-                <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
-                  Points
-                </th>
-                <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
-                  Time
-                </th>
-                <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
-                  Complexity
-                </th>
-                <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
-                  Task Risk/Uncertainty
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {storyPointDescriptions.map((row, index) => (
-                <tr key={index} className="hover:bg-gray-100">
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {row.points}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {row.time}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {row.complexity}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {row.risk}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div className="overflow-x-auto">
+      <Card className="mb-4">
+        <Collapsible>
+          <CardHeader>
+            <CollapsibleTrigger className="w-full text-left text-xl font-bold">
+              Definition of Done ⬇️
+            </CollapsibleTrigger>
+          </CardHeader>
+
+          <CollapsibleContent>
+            <CardContent>
+              <CardDescription className="text-sm text-gray-700">
+                The "Definition of Done" is a shared understanding among the
+                team of what it means for work to be considered complete. This
+                includes the completion of all tasks, successful passing of all
+                tests, and adherence to all relevant standards and guidelines.
+                This ensures consistency and quality in the deliverables.
+              </CardDescription>
+              <CardDescription className="mt-2 text-sm text-gray-700">
+                For example, a feature is considered done when:
+                <ul className="list-inside list-disc">
+                  <li>All linting, unit and integration tests are passing.</li>
+                  <li>
+                    A merge request (MR) is created, reviewed, and approved
+                    through Peer and Lead Review.
+                  </li>
+                  <li>
+                    The code is merged into the master branch or a dedicated
+                    epic branch.
+                  </li>
+                </ul>
+              </CardDescription>
+            </CardContent>
+          </CollapsibleContent>
+        </Collapsible>
+      </Card>
+      <Card className="mb-4">
+        <Collapsible>
+          <CardHeader>
+            <CollapsibleTrigger className="w-full text-left text-xl font-bold">
+              Story Point Descriptions ⬇️
+            </CollapsibleTrigger>
+          </CardHeader>
+          <CollapsibleContent id="table-content">
+            <CardContent>
+              <table className="min-w-full border-collapse bg-white">
+                <thead>
+                  <tr>
+                    <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
+                      Points
+                    </th>
+                    <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
+                      Time
+                    </th>
+                    <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
+                      Complexity
+                    </th>
+                    <th className="border-b-2 border-gray-300 bg-gray-100 px-4 py-2 text-left text-sm leading-4 text-gray-600">
+                      Task Risk/Uncertainty
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {storyPointDescriptions.map((row, index) => (
+                    <tr key={index} className="hover:bg-gray-100">
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        {row.points}
+                      </td>
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        {row.time}
+                      </td>
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        {row.complexity}
+                      </td>
+                      <td className="border-b border-gray-300 px-4 py-2">
+                        {row.risk}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </CardContent>
+          </CollapsibleContent>
+        </Collapsible>
+      </Card>
     </div>
   );
 };
